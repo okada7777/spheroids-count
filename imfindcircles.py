a89b730ec9e9415d92f8cbb5b814311beccc7355
okada7777 <okadazoku@gmail.com>
2024-05-24 15:40:02 +0900

    find_circles

--- imfindcircles.py
+++ imfindcircles.py
@@ -3,8 +3,8 @@ import numpy as np
 
 A = cv2.imread('spheroids_Snapshot_20240406_01_rgb.ome');
 cv2.imshow(A)
-Rmin = 50;
-Rmax = 800;
-[centersBright, radiiBright] = cv2.imfindcircles(A,[Rmin Rmax],'ObjectPolarity','bright');
-[centersDark, radiiDark] = cv2.imfindcircles(A,[Rmin Rmax],'ObjectPolarity','dark');
-cv2.viscircles(centersBright, radiiBright,'Color','b');
+Rmin = 50
+Rmax = 800
+[centersBright, radiiBright] = cv2.imfindcircles(A,[Rmin Rmax],'ObjectPolarity','bright')
+[centersDark, radiiDark] = cv2.imfindcircles(A,[Rmin Rmax],'ObjectPolarity','dark')
+cv2.viscircles(centersBright, radiiBright,'Color','b')
